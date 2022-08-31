0ce0ad28450194cb6bf0977ed588e7dfcb1f9df8
Ovcharov,Ivan <450754@student.fontys.nl>
2022-08-31 21:56:52 +0200

    update: working on slight changes in discordbot

--- discordBot.py
+++ discordBot.py
@@ -2,7 +2,7 @@ from math import prod
 import discord
 from main import scrapeProductInformation
 
-token = "MTAxNDU2NjUwNDA5NzkyMzE0Mg.GHNhaS.nqewLaj3e8YRMUSZkjMa9U8XzDv6GUVPZkfSmw"
+token = "MTAxNDU2NjUwNDA5NzkyMzE0Mg.Gb3eE_.Hg2ipgO2Op60HAAFhHg8NukFE6Zp26ZhHRGX2g"
 
 client = discord.Client(intents=discord.Intents.default())
 
@@ -16,28 +16,32 @@ async def on_message(message):
         query = message.content.replace('!stockxquery', '')
         product = scrapeProductInformation(query)
 
-        embed = discord.Embed(
-            title=product['title'],
-            url = "https://stockx.com/en-gb/" + product['urlKey']
-        )
+    embed = discord.Embed(
+        title = "HI"
+    )
+    embed = discord.Embed(
+        title=product['title'],
+        url = "https://stockx.com/en-gb/" + product['urlKey']
+    )
 
-        embed.set_thumbnail(
-            url = product['media']['imageUrl']
-        )
-        embed.add_field(
-            name = "Has asks",
-            value = f"{product['market']['hasAsks']}"
-        )
-        embed.add_field(
-            name = "Lowest ask & lowest ask size",
-            value = f"Lowest ask: {product['market']['lowestAsk']} | Lowest ask size {product['market']['lowestAskSize']}"
-        )
+    embed.set_thumbnail(
+        url = product['media']['imageUrl']
+    )
+    embed.add_field(
+        name = "Has asks",
+        value = f"{product['market']['hasAsks']}"
+    )
+    embed.add_field(
+        name = "Lowest ask & lowest ask size",
+        value = f"Lowest ask: {product['market']['lowestAsk']} | Lowest ask size {product['market']['lowestAskSize']}"
+    )
 
-        embed.add_field(
-            name = "Last sale & sale size",
-            value = f"Sale: {product['market']['lastSale']} | Last sale size: {product['market']['lastSaleSize']}"
-        )
+    embed.add_field(
+        name = "Last sale & sale size",
+        value = f"Sale: {product['market']['lastSale']} | Last sale size: {product['market']['lastSaleSize']}"
+    )
+    await message.channel.send(embed=embed)
 
-        await message.channel.send(embed=embed)
+
 
 client.run(token)
